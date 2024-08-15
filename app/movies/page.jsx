b9349c6d66666013fecbacb87e368ee96f06079e
DILPREET1910 <dsahney1910@gmail.com>
2024-08-15 18:05:34 +0530

    movies get data from api

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -6,98 +6,21 @@ import {
   CardDescription,
 } from "@components/ui/card";
 
-export default function MoviesRoot() {
-  const movies = [
-    {
-      title: "Movie 1",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 2",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    // Add more movies here...
-    {
-      title: "Movie 3",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 4",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 5",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 6",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 7",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 8",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 9",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    // Add more movies here...
-    {
-      title: "Movie 10",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 11",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 12",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-    {
-      title: "Movie 13",
-      description: "This is another movie",
-      poster:
-        "https://plus.unsplash.com/premium_photo-1658526991238-f34a36d968fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fHww",
-    },
-  ];
+export default async function MoviesRoot() {
+  const data = await fetch(
+    "https://9390-103-187-81-72.ngrok-free.app/api/movies",
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error("Error:", error));
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-      {movies.map((movie, index) => {
+      {data.map((movie, index) => {
         return (
           <Card className="w-full max-w-sm" key={index}>
             <div className="aspect-w-4 aspect-h-5 relative">
               <img
-                src={movie.poster}
+                src={movie.image}
                 alt="Product"
                 width={400}
                 height={500}
@@ -107,7 +30,7 @@ export default function MoviesRoot() {
             </div>
             <CardHeader className="grid gap-1 p-4">
               <CardTitle>{movie.title}</CardTitle>
-              <CardDescription>{movie.description}</CardDescription>
+              <CardDescription>{movie.genre}</CardDescription>
             </CardHeader>
             <CardContent className="p-4">
               <button className="z-10 mt-4 text-black py-2 px-4 rounded-full black_btn border border-black bg-transparent">
